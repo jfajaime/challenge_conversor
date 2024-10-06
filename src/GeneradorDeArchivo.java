ffb8a103582bb0d805190912cdb51a885a751ec0
jfajaime <jfajaime@hotmail.com>
2024-10-05 21:52:03 -0300

    pruebas

--- src/GeneradorDeArchivo.java
+++ src/GeneradorDeArchivo.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class GeneradorDeArchivo {
     public void guardarJson(Moneda moneda) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        try (FileWriter escritura = new FileWriter("Moneda base: " + moneda.base_code() + ".json")) {
+        try (FileWriter escritura = new FileWriter(moneda.base_code() + ".json")) {
             escritura.write(gson.toJson(moneda));
         } catch (IOException e) {
             System.out.println("Error al guardar el archivo JSON: " + e.getMessage());
